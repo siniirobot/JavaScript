debdbf5f488bb6545232af211a395286ff11c1de
siniirobotRab <siniirobot@gmail.com>
2019-07-01 11:28:09 +0700

    ArrayWork:  закончил работу над массивами.

--- ArrayWork/src/ArrayWork/ArrayWork.js
+++ ArrayWork/src/ArrayWork/ArrayWork.js
@@ -1,35 +1,54 @@
-function reverse(){
-    var numberArray = [1,2,3,4,5];
+var numberArray = [1, 2, 3, 4, 5];
 
-    return numberArray.sort(function reversSort(a,b){
-        return b-a;
-        });
+
+function reverse() {
+    numberArray.sort(function reversSort(a, b) {
+        return b - a;
+    });
 }
 
-console.log(reverse);
+reverse();
+console.log(numberArray);
 
-numberArray = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
+numberArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
 
-function subArrayFirstFive(){
-    var firstFive = numberArray.slice(0,5);
+function subArrayFirstFive() {
+    return numberArray.slice(0, 5);
 }
 
-console.log(subArrayFirstFive);
+console.log(subArrayFirstFive());
 
-function subArrayLastFive(){
-    var lastFive = numberArray.slice(10);
+function subArrayLastFive() {
+    return numberArray.slice(10);
 }
 
-console.log(lastFive);
+console.log(subArrayLastFive());
 
-function sum(){
-    var sum = numberArray.reduce(function(sum,current) {
-              if(current%2 == 0) {
-                return sum + current;
-                }
-                return sum;
-              },0);
+function sumEvenNumbers() {
+    return numberArray.reduce(function (sum, current) {
+        if (current % 2 === 0) {
+            return sum + current;
+        }
+        return sum;
+    }, 0);
 }
 
 
-console.log(sum);
+console.log(sumEvenNumbers());
+
+function squaresOfEvenNumbersList() {
+    var hundred = [];
+    var squaresEvenNumbersList = [];
+
+    for (var i = 0; i < 100; i++) {
+        hundred[i] = i + 1;
+        if (hundred[i] % 2 === 0) {
+            squaresEvenNumbersList.push(hundred[i] * 2);
+        }
+    }
+
+    return squaresEvenNumbersList;
+}
+
+
+console.log(squaresOfEvenNumbersList());
