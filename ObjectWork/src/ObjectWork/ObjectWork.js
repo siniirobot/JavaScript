7ca64253d6d0dd345b2911d880446a70fafdbfc0
siniirobotRab <siniirobot@gmail.com>
2019-07-04 15:18:39 +0700

    Object Work 1. п.3 не исправлен +-
    2. cites - опечатка +
    
    3. Здесь лучше не переносить строку:
    http://joxi.ru/xAeel90hRa0q7A
    
    4. Если переменная - массив, то имя должно быть во множественном числе
    
    5. Функция в map должна быть чистой - она не должна менять ничего снаружи себя
    
    6. Лишние пустые строки:
    http://joxi.ru/a2Xey8QhwKNWBA
    
    7. cityInfo - неподходящее по смыслу имя
    
    8. Код не отформатирован
    
    9. Плохо использовать += как выражение

--- ObjectWork/src/ObjectWork/ObjectWork.js
+++ ObjectWork/src/ObjectWork/ObjectWork.js
@@ -1,83 +1,83 @@
 var countries = [
-    countryA = {
+    {
         name: "countryA",
-        cites: [
-            cityA = {
+        cities: [
+            {
                 name: "cityA",
                 population: 5000
             },
-            cityB = {
+            {
                 name: "cityB",
                 population: 8000
             },
-            cityC = {
+            {
                 name: "cityC",
                 population: 9000
             },
-            cityD = {
+            {
                 name: "cityD",
                 population: 1000
             }
         ]
     },
-    countryB = {
+    {
         name: "countryB",
-        cites: [
-            cityE = {
+        cities: [
+            {
                 name: "cityE",
                 population: 2000
             },
-            cityF = {
+            {
                 name: "cityF",
                 population: 80000
             }
         ]
     },
-    countryC = {
+    {
         name: "countryC",
-        cites: [
-            cityG = {
+        cities: [
+            {
                 name: "cityG",
                 population: 12000
             },
-            cityH = {
+            {
                 name: "cityH",
                 population: 8000
             },
-            cityI = {
+            {
                 name: "cityI",
                 population: 9000
             },
-            cityJ = {
+            {
                 name: "cityJ",
                 population: 1000
             }
         ]
     },
-    countryD = {
+    {
         name: "countryD",
-        cites: [
-            cityK = {
+        cities: [
+            {
                 name: "cityK",
                 population: 5000
             },
-            cityL = {
+            {
                 name: "cityL",
                 population: 5000
             },
-            cityM = {
+            {
                 name: "cityM",
                 population: 5000
             },
-            cityN = {
+            {
                 name: "cityN",
                 population: 5000
             },
-            cityO = {
+            {
                 name: "cityO",
                 population: 5000
             },
-            cityP = {
+            {
                 name: "cityP",
                 population: 5000
             }]
@@ -93,10 +93,10 @@ function getLargestCountry(array) {
             if (largestCountry.length === 0) {
                 largestCountry.push(value)
             } else {
-                if (value.cites.length > largestCountry[0].cites.length) {
+                if (value.cities.length > largestCountry[0].cities.length) {
                     largestCountry = [];
                     largestCountry.push(value);
-                } else if (value.cites.length === largestCountry[0].cites.length) {
+                } else if (value.cities.length === largestCountry[0].cities.length) {
                     largestCountry.push(value);
                 }
             }
@@ -113,10 +113,10 @@ function getCountryInformation() {
     countries
         .forEach(function (value) {
 
-            cityInfo[value.name] = value.cites
+            cityInfo[value.name] = value.cities
                 .reduce(function (previousValue, currentValue) {
                     return previousValue += currentValue.population;
-                },0);
+                }, 0);
         });
 
     return cityInfo;
