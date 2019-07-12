83d3b754a6f7f087bce678c8aa2da966e23a997a
siniirobotRab <siniirobot@gmail.com>
2019-07-12 08:49:40 +0700

    Temperature 1. Пропускает только температуру а не всякую ерунду

--- JavaScriptTemperature/src/ru/academIt/gorbunov/Temperature/Temperature.js
+++ JavaScriptTemperature/src/ru/academIt/gorbunov/Temperature/Temperature.js
@@ -4,19 +4,16 @@ document.addEventListener("DOMContentLoaded",function () {
     var inputTempButton = document.getElementById("input-temp-button");
     inputTempButton.addEventListener("click",function () {
         var newText = inputTemp.value;
-        if (!/[+-]?([0-9]*[.])?[0-9]+/.test(newText)){
+        if (!/^[+-]?([0-9]*[.])?[0-9]+$/.test(newText)){
             inputTemp.value = "Введите температуру в градсуах C";
             return;
         }
-        /*if (isNaN(parseFloat(newText) && isNaN(newText))) {
-            inputTemp.value = "Введите температуру в градсуах C";
-            return;
-        }*/
+        var tempInCelsius = parseFloat(newText);
 
         document.getElementById("output-fahrenheit").innerHTML
-            = "Темепература в фаренгейт - " + ((newText * 9/5) + 32);
+            = "Темепература в фаренгейт - " + ((tempInCelsius * 9/5) + 32);
 
         document.getElementById("output-kelvin").innerHTML
-            = "Темепература в кельвин - " + (newText + 273.15);
+            = "Темепература в кельвин - " + (tempInCelsius + 273.15);
     })
 });
