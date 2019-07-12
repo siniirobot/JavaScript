eee87198b1312638d9db85939c2c45b0d96f71e3
siniirobotRab <siniirobot@gmail.com>
2019-07-12 15:23:41 +0700

    ToDoList 1.Сделал рекурсивный вызов функции вызова редактирования.

--- TODOList/src/ru.acdemIt.gomunov.ToDoList/js.js
+++ TODOList/src/ru.acdemIt.gomunov.ToDoList/js.js
@@ -1,4 +1,46 @@
 document.addEventListener("DOMContentLoaded", function () {
     var inputTemp = document.getElementById("input");
-    var inputTempButton = document.getElementById("button")
+    var inputTempButton = document.getElementById("input-button");
+    var toDoList = document.getElementById("todo-list");
+
+    inputTempButton.addEventListener("click", function () {
+        var newText = inputTemp.value;
+
+        if (newText.trim() === "") {
+            return;
+        }
+
+        var li = document.createElement("li");
+
+        function createLi(text) {
+            li.innerHTML = "<span></span><button type='button'>Стереть</button><button type='button'>Редактировать</button>";
+
+            li.children[0].textContent = text + "   ";
+            li.children[1].addEventListener("click", function () {
+                toDoList.removeChild(li);
+            });
+
+            li.children[2].addEventListener("click", function () {
+                li.innerHTML = "<input type='text' placeholder='Введите правки'><button type='button'>Ввод</button>";
+                var editButton = li.children[1];
+
+                editButton.addEventListener("click", function () {
+                    var newEditText = li.children[0].value;
+
+                    if (newEditText.trim() === "") {
+                        createLi(text);
+                        return;
+                    }
+
+                    createLi(newEditText);
+                });
+            });
+        }
+
+        createLi(newText);
+
+        toDoList.appendChild(li);
+
+        inputTemp.value = "";
+    });
 });
