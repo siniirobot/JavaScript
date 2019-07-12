a67e2d86ac1a6e73d3c1fdbe673ada8a1287d236
siniirobotRab <siniirobot@gmail.com>
2019-07-12 11:40:37 +0700

    Temperature 1. Сделал округление

--- JavaScriptTemperature/src/ru/academIt/gorbunov/Temperature/Temperature.js
+++ JavaScriptTemperature/src/ru/academIt/gorbunov/Temperature/Temperature.js
@@ -1,19 +1,21 @@
-document.addEventListener("DOMContentLoaded",function () {
+document.addEventListener("DOMContentLoaded", function () {
     var inputTemp = document.getElementById("input");
 
     var inputTempButton = document.getElementById("input-temp-button");
-    inputTempButton.addEventListener("click",function () {
+    inputTempButton.addEventListener("click", function () {
         var newText = inputTemp.value;
-        if (!/^[+-]?([0-9]*[.])?[0-9]+$/.test(newText)){
-            inputTemp.value = "Введите температуру в градсуах C";
+
+        if (!(!isNaN(parseFloat(newText)) && isFinite(newText))) {
+            inputTemp.value = "";
             return;
         }
+
         var tempInCelsius = parseFloat(newText);
 
         document.getElementById("output-fahrenheit").innerHTML
-            = "Темепература в фаренгейт - " + ((tempInCelsius * 9/5) + 32);
+            = "Темепература в фаренгейт - " + ((tempInCelsius * 9 / 5) + 32).toFixed(2);
 
         document.getElementById("output-kelvin").innerHTML
-            = "Темепература в кельвин - " + (tempInCelsius + 273.15);
+            = "Темепература в кельвин - " + (tempInCelsius + 273.15).toFixed(2);
     })
 });
