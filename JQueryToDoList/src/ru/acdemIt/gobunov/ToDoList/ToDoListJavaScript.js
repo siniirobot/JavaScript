aeddd544055781bf56a915d4b7c101b6e9c04ce3
siniirobotRab <siniirobot@gmail.com>
2019-08-08 16:01:13 +0700

    JQueryToDoList 1.Дописал диалог

--- JQueryToDoList/src/ru/acdemIt/gobunov/ToDoList/ToDoListJavaScript.js
+++ JQueryToDoList/src/ru/acdemIt/gobunov/ToDoList/ToDoListJavaScript.js
@@ -1,4 +1,34 @@
 $(document).ready(function () {
+    $("#confirm-dialog").dialog({
+        autoOpen: false,
+        modal: true,
+        show: {effect: "blind", duration: 800},
+        hide: {effect: "explode", duration: 800}
+    });
+
+    function addButtonToConfirmDialog(li) {
+        $("#confirm-dialog").dialog(
+            {
+                buttons: [
+                    {
+                        text: "Да",
+                        icon: "ui-icon-trash",
+                        click: function () {
+                            li.remove();
+                            $(this).dialog("close");
+                        }
+                    },
+                    {
+                        text: "Нет",
+                        icon: "ui-icon-cancel",
+                        click: function () {
+                            $(this).dialog("close");
+                        }
+                    }
+                ]
+            });
+    }
+
     var inputTemp = $("#input");
     var inputTempButton = $("#input-button");
     var toDoList = $(".todo-list");
@@ -17,15 +47,16 @@ $(document).ready(function () {
         inputTemp.val("");
 
         function initLi(text) {
-
             li.html("<span></span>" +
                 "<button type='button'>Стереть</button>" +
                 "<button type='button'>Редактировать</button>");
 
             li.children().eq(0).text(text);
             li.children().eq(1).click(function () {
-                li.remove();
+                addButtonToConfirmDialog(li);
+                $("#confirm-dialog").dialog("open");
             });
+
 
             li.children().eq(2).click(function () {
                 li.html("<input type='text' value='" + text + "'>" +
