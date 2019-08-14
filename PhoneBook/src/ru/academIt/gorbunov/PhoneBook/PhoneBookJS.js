f6e79a3f087c78ea91e71553d7f9a0b4eb398405
siniirobot <siniirobot@gmail.com>
2019-08-14 23:30:54 +0700

    phoneBook 1.Закончил валидацию 2. делаю удаление

--- PhoneBook/src/ru/academIt/gorbunov/PhoneBook/PhoneBookJS.js
+++ PhoneBook/src/ru/academIt/gorbunov/PhoneBook/PhoneBookJS.js
@@ -1,13 +1,20 @@
 $(function () {
     var tableBody = $("#table-body");
     var addButton = $("#add-button");
+    var index = 1;
 
     addButton.click(function () {
-        $('#add-person-modal').modal('show');
-        $("#phone").mask("+7(999)999-99-99");
+        var addPersonModal = $('#add-person-modal');
         var form = document.querySelector('.needs-validation');
-
+        var surname = $("#surname");
+        var name = $("#name");
+        var phone = $("#phone");
         var addPersonButton = $("#button-save-data");
+
+        addPersonModal.modal('show');
+        phone.mask("+7(999)999-99-99");
+        form.reset();
+
         addPersonButton.click(function (event) {
             if (form.checkValidity() === false) {
                 event.preventDefault();
@@ -15,16 +22,22 @@ $(function () {
                 form.classList.add('was-validated');
             } else {
                 var tr = $("<tr>");
-                var surname = $("#surname");
-                var name = $("#name");
-                var phone = $("#phone");
-                tr.html("<td>" + surname.val() + "</td><td>" + name.val() + "</td><td>" + phone.val() + "</td>");
+                tr.html("<td>" + index + "</td>" +
+                    "<td>" + surname.val() + "</td>" +
+                    "<td>" + name.val() + "</td>" +
+                    "<td>" + phone.val() + "</td>" +
+                    "<td><button type='button' class='btn bg-danger'>Удалить</button></td>");
+                var deleteButton = tr.children().eq(4).children().eq(0);
+                deleteButton.click(function () {
+                    tr.remove();
+                    index = 1;
+                    tableBody.each(function (i, elem) {
+                        elem.children(0).value(index++);
+                    })
+                });
+                index++;
                 tableBody.append(tr);
-                surname.text("");
-                name.text("");
-                phone.text();
-                $('#add-person-modal').modal('hide');
-
+                addPersonModal.modal('hide');
             }
         });
     });
