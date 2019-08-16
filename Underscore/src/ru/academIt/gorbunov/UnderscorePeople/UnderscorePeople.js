0c5ad08b17d5110b7ae10c5e440be427e7289760
siniirobotRab <siniirobot@gmail.com>
2019-08-16 13:41:18 +0700

    Underscore 1.Создыл масив людей

---
+++ Underscore/src/ru/academIt/gorbunov/UnderscorePeople/UnderscorePeople.js
@@ -0,0 +1,56 @@
+document.addEventListener("DOMContentLoaded", function () {
+    var peopls = [
+        {
+            age:25,
+            name: "Andy",
+            lastName:"Kendy"
+        },
+        {
+            age:35,
+            name: "David",
+            lastName:"Mavid"
+        },
+        {
+            age:15,
+            name: "Gorge",
+            lastName:"Ortg"
+        },
+        {
+            age:45,
+            name: "Angelina",
+            lastName:"Milina"
+        },
+        {
+            age:67,
+            name: "Mark",
+            lastName:"Cucamber"
+        },
+        {
+            age:23,
+            name: "Maria",
+            lastName:"Shwartz"
+        },
+        {
+            age:99,
+            name: "Kate",
+            lastName:"Ostin"
+        },
+        {
+            age:11,
+            name: "Nikolai",
+            lastName:"Hypo"
+        },
+        {
+            age:4,
+            name: "Andrew",
+            lastName:"Dahno"
+        },
+        {
+            age:46,
+            name: "Oleg",
+            lastName:"Vlasov"
+        }
+    ];
+
+
+});
