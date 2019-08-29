b0ae3406b1b23d006685503f30de1f793ade4f60
siniirobotRab <siniirobot@gmail.com>
2019-08-29 09:05:56 +0700

    PhoneBook Добрый день!
    Вопрос все же не понял.
    У меня вроде все нормально работает.
    
    Но, возможно, дело в этом.
    
    Был такой код:
    http://joxi.ru/4AkOwPeio91RDA
    Этот код навешивает обработчик на клик на кнопку.
    Но этот код выполнялся при каждом клике на addButton.
    Т.е. каждый раз на ту же кнопку навешивался еще 1 обработчик.
    
    1. Можно обойтись без each, т.к. сеттеры применяются ко всему набору:
    http://joxi.ru/BA04ekvCMawN8A
    
    2. Нужно использовать единообразный вариант кавычек +
    
    3. Диалог подтверждения должен предлагать 2 варианта - да и нет
    
    4. OK - лучше обе буквы сделать заглавными в тексте кнопки
    
    5. Сейчас если создать контакт, и после этого открыть диалог создания еще раз, то сразу будет применена валидация, что все поля пустые
    
    6. Страница сейчас подвержена HTML-инъекции
    
    7. Плохо использовать ++ как выражение
    
    8. tr.children().eq(4).children().eq(0).
    Можно не делать такие сложные вещи.
    Можно было сделать, например, tr.find(".btn")

--- PhoneBook/src/ru/academIt/gorbunov/PhoneBook/PhoneBookJS.js
+++ PhoneBook/src/ru/academIt/gorbunov/PhoneBook/PhoneBookJS.js
@@ -4,35 +4,35 @@ $(function () {
     var index = 1;
 
     addButton.click(function () {
-        var addPersonModal = $('#add-person-modal');
-        var form = document.querySelector('.needs-validation');
+        var addPersonModal = $("#add-person-modal");
+        var form = document.querySelector(".needs-validation");
         var surname = $("#surname");
         var name = $("#name");
         var phone = $("#phone");
         var savePersonButton = $("#button-save-data");
 
-        addPersonModal.modal('toggle');
+        addPersonModal.modal("toggle");
         phone.mask("+7(999)999-99-99");
 
         savePersonButton.click(function () {
             if (form.checkValidity() === false) {
                 event.preventDefault();
                 event.stopPropagation();
-                form.classList.add('was-validated');
+                form.classList.add("was-validated");
             } else {
                 var tr = $("<tr>").html(
-                    "<td class='id'><p>" + index + "</p></td>" +
+                    "<td class=\"id\"><p>" + index + "</p></td>" +
                     "<td><p>" + surname.val() + "</p></td>" +
                     "<td><p>" + name.val() + "</p></td>" +
                     "<td><p>" + phone.val() + "</p></td>" +
-                    "<td><button type='button' class='btn bg-danger'>Удалить</button></td>");
+                    "<td><button type=\"button\" class=\"btn bg-danger\">Удалить</button></td>");
                 var deleteButton = tr.children().eq(4).children().eq(0);
 
                 deleteButton.click(function () {
                     var confirmDeleteModal = $("#delete-person-modal");
                     var confirmDeleteButton = $("#confirm-delete-button");
 
-                    confirmDeleteModal.modal('toggle');
+                    confirmDeleteModal.modal("toggle");
 
                     confirmDeleteButton.click(function () {
                         tr.remove();
@@ -41,7 +41,7 @@ $(function () {
                         $("tr", tableBody).each(function (i, val) {
                             $(val).children().eq(0).text(index++);
                         });
-                        confirmDeleteModal.modal('toggle');
+                        confirmDeleteModal.modal("toggle");
                     });
                 });
                 index++;
@@ -50,7 +50,7 @@ $(function () {
                 $("input", form).each(function (i, val) {
                     $(val).val("");
                 });
-                addPersonModal.modal('toggle');
+                addPersonModal.modal("toggle");
             }
         });
     });
